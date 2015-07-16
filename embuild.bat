9a79cecd47d861282b4316ce3beaccb757426baa
Faiz Ilham <indigo.republica@gmail.com>
2015-07-16 21:15:35 +0700

    change to SDL2

--- embuild.bat
+++ embuild.bat
@@ -4,4 +4,4 @@
 
 set OBJ=src/main.c src/chip8.c src/display.c
 
-emcc -Isrc %OBJ% -o web/chip8.js
+emcc -Isrc %OBJ% -s USE_SDL=2 -o web/chip8.js
--- src/display.c
+++ src/display.c
@@ -3,7 +3,9 @@
 char display[64][32];
 SDL_Rect pixel[64][32];
 const int width = 640, height = 320, size=10;
-SDL_Surface *screen = NULL;
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 
 static inline void init_pixel(){
 	for (int x = 0; x < 64; ++x){
@@ -20,32 +22,39 @@ static inline void init_pixel(){
 
 inline int init_display(){
 	if (SDL_Init(SDL_INIT_VIDEO) == 0){
-		if ((screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE | SDL_DOUBLEBUF))!=NULL){
+		if ((window = SDL_CreateWindow("Chip8js", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN)) == NULL) return 0;
+		if ((renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)) == NULL) return 0;
+
+		//if ((screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE | SDL_DOUBLEBUF))!=NULL){
 			init_pixel();
 			return 1;
-		}
+		//}
 	}
 
 	return 0;
 }
 
 inline void shutdown_display(){
-	SDL_FreeSurface(screen);
+	//SDL_FreeSurface(screen);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
 
  
 inline void draw() {
 	// simple draw update
-	SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
+	SDL_SetRenderDrawColor(renderer, 0,0,0,255);
+	SDL_RenderClear(renderer);
 
 	for (int x = 0; x < 64; ++x){
 		for (int y = 0; y < 32; ++y){
 			if (display[x][y]){
-				SDL_FillRect(screen, &pixel[x][y], SDL_MapRGB(screen->format, 255, 255, 255));
+				SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+				SDL_RenderFillRect(renderer, &pixel[x][y]);
 			}
 		}
 	}
-	SDL_Flip(screen);
+	SDL_RenderPresent(renderer);
 }
 
