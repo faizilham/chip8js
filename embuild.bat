5787862b8b53d0fadb85dfe2dd93168c82e6d6d5
Faiz Ilham <indigo.republica@gmail.com>
2015-07-26 15:54:50 +0700

    edit builder script

--- embuild.bat
+++ embuild.bat
@@ -2,6 +2,7 @@
 :: disable cygwin temporarily
 @set PATH=%PATH:cygwin=cygwin_unused%
 
+:: notes, 02 and 03 optimization not working
 set OBJ=src/main.c src/chip8.c src/media.c
 set OPT= -s USE_SDL=2 -s EXPORTED_FUNCTIONS="['_main', '_stop_loop']" -O1 --embed-file beep.wav
 
--- embuild.sh
+++ embuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# notes, 02 and 03 optimization not working
 OBJ=src/main.c src/chip8.c src/media.c
+OPT= -s USE_SDL=2 -s EXPORTED_FUNCTIONS="['_main', '_stop_loop']" -O1 --embed-file beep.wav
 
-emcc -Isrc $OBJ -s USE_SDL=2 -o web/chip8.js
+emcc -Isrc $OBJ $OPT -o web/chip8.js
